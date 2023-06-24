ca0623c3a461ec5a741a0c9d32d8c4974fbaa8c1
Neha Nupur <110279038+neha-nupur@users.noreply.github.com>
2023-06-25 02:02:57 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav>
       <img src={reactLogo} className="nav--icon" />
       <h3 className="nav--logo_text">ReactFacts</h3>
-      <h4 className="nav--title">React Course - Project 1</h4>
+      <h4 className="nav--title">React Project 1 </h4>
       <a className="nav--title" href="https://react.dev/">Learn More About React</a>
     </nav>
   );
